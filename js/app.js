479bd729419970d728c8f9d683008db577903b00
Babajide Fowotade <babajide.fowotade@andela.co>
2015-05-24 05:11:19 +0100

    Initializing repo for gh pages

---
+++ js/app.js
@@ -0,0 +1,17 @@
+Reveal.initialize({
+
+    // Parallax background image
+    parallaxBackgroundImage: 'img/optimized-bg.jpg', // e.g. "https://s3.amazonaws.com/hakim-static/reveal-js/reveal-parallax-1.jpg"
+
+    // Parallax background size
+    parallaxBackgroundSize: '', // CSS syntax, e.g. "2100px 900px" - currently only pixels are supported (don't use % or auto)
+
+    // Amount of pixels to move the parallax background per slide step,
+    // a value of 0 disables movement along the given axis
+    // These are optional, if they aren't specified they'll be calculated automatically
+    parallaxBackgroundHorizontal: 90,
+    parallaxBackgroundVertical: 50,
+
+    transition: 'convex'
+
+});
